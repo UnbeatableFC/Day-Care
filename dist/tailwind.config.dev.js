f60ff3d8f7d4232ed12ed4928229589f2228fe64
UnbeatableFC <wisdommpam99@gmail.com>
2025-01-08 19:03:16 +0100

    first

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+//# sourceMappingURL=tailwind.config.dev.js.map
