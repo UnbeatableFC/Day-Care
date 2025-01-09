74f27eb1c458f74b45c210226d2b0610ddf0f64a
UnbeatableFC <wisdommpam99@gmail.com>
2025-01-09 14:32:57 +0100

    finished

--- dist/tailwind.config.dev.js
+++ dist/tailwind.config.dev.js
@@ -4,6 +4,12 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      'mob': '641px',
+      // => @media (min-width: 640px) { ... }
+      'tab': '769px' // => @media (min-width: 728px) { ... }
+
+    },
     extend: {}
   },
   plugins: []
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      'mob': '641px',
+      // => @media (min-width: 640px) { ... }
+
+      'tab': '769px',
+      // => @media (min-width: 728px) { ... }
+    }, 
     extend: {},
   },
   plugins: [],
