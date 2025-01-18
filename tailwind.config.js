42a593753ef1a07f28801793fc280d51e7a0e0fb
UnbeatableFC <wisdommpam99@gmail.com>
2025-01-18 11:22:59 +0100

    next

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-   
+
+    
     extend: {
+
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'], },
+
       screens: {
         'mob': '641px',
         // => @media (min-width: 640px) { ... }
